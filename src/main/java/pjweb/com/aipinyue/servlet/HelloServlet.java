9564d8a27a920787848a3a305551f6e147ffd468
surersen <surersen@163.com>
2018-12-08 12:11:06 +0800

    Update HelloServlet.java

--- src/main/java/pjweb/com/aipinyue/servlet/HelloServlet.java
+++ src/main/java/pjweb/com/aipinyue/servlet/HelloServlet.java
@@ -28,6 +28,7 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at:adsadas ").append(request.getContextPath());
+		response.getWriter().append("Served at:adsadas ").append(request.getContextPath());
 		
 	}
 
